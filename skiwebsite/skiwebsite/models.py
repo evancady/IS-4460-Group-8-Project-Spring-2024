8ebe2e7e835fa735bad3ea2166b05eacb2b66ff0
Evan Cady <143289907+evancady@users.noreply.github.com>
2024-04-03 22:22:34 -0600

    added views, customer and employee templates, serializers, urls files, forms file.

--- skiwebsite/skiwebsite/models.py
+++ skiwebsite/skiwebsite/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Payment(models.Model):
     pmt_id = models.AutoField(primary_key=True)
     date = models.DateField()
@@ -7,17 +8,20 @@ class Payment(models.Model):
     cust_id = models.ForeignKey('Customer', on_delete=models.CASCADE)
     credit_card = models.CharField(max_length=19)
 
+
 class Employee(models.Model):
     emp_id = models.AutoField(primary_key=True)
     last = models.CharField(max_length=50)
     first = models.CharField(max_length=50)
     position = models.CharField(max_length=50)
 
+
 class Customer(models.Model):
     cust_id = models.AutoField(primary_key=True)
     first = models.CharField(max_length=50)
     last = models.CharField(max_length=50)
     address = models.TextField()
+
 
 class Order(models.Model):
     order_id = models.AutoField(primary_key=True)
@@ -26,6 +30,7 @@ class Order(models.Model):
     date = models.DateField()
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
     pmt_id = models.OneToOneField('Payment', on_delete=models.CASCADE)
+
 
 class Product(models.Model):
     prod_id = models.AutoField(primary_key=True)
@@ -37,11 +42,13 @@ class Product(models.Model):
     ski_name = models.CharField(max_length=100)
     dimensions = models.CharField(max_length=100)
 
+
 class Shipping(models.Model):
     ship_id = models.AutoField(primary_key=True)
     ship_date = models.DateField()
     delivery_date = models.DateField()
     cost = models.DecimalField(max_digits=10, decimal_places=2)
+
 
 class Return(models.Model):
     return_id = models.AutoField(primary_key=True)
@@ -49,12 +56,14 @@ class Return(models.Model):
     order = models.ForeignKey('Order', on_delete=models.CASCADE)
     quantity = models.IntegerField()
 
+
 class RawMaterial(models.Model):
     material_id = models.AutoField(primary_key=True)
     cast = models.CharField(max_length=100)
     date_purchased = models.DateField()
     quantity = models.IntegerField()
     material_name = models.CharField(max_length=100)
+
 
 class OrderLine(models.Model):
     line_id = models.AutoField(primary_key=True)
