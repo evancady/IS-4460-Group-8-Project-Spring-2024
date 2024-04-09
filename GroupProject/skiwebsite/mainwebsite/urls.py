5dfb34c6d5bd6cf8c350c69d5970e0738fa4b6cf
JJNads <122838039+JJNads@users.noreply.github.com>
2024-04-08 20:27:21 -0600

    Add files via upload

--- GroupProject/skiwebsite/mainwebsite/urls.py
+++ GroupProject/skiwebsite/mainwebsite/urls.py
@@ -1,19 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
-from skiwebsite import views
+from mainwebsite import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # Employee URLs
-    path('employees/', views.EmployeeListView.as_view(), name='employee_list'),
-    path('employees/add/', views.EmployeeAddView.as_view(), name='employee_add'),
-    path('employees/edit/<int:pk>/', views.EmployeeUpdateView.as_view(), name='employee_edit'),
-    path('employees/delete/<int:pk>/', views.EmployeeDeleteView.as_view(), name='employee_delete'),
+    path('employees/', views.EmployeeList.as_view(), name='employee_list'),
+    path('employees/add/', views.EmployeeAdd.as_view(), name='employee_add'),
+    path('employees/edit/<int:pk>/', views.EmployeeUpdate.as_view(), name='employee_edit'),
+    path('employees/delete/<int:pk>/', views.EmployeeDelete.as_view(), name='employee_delete'),
     # Customer URLs
-    path('customers/', views.CustomerListView.as_view(), name='customer_list'),
-    path('customers/add/', views.CustomerAddView.as_view(), name='customer_add'),
-    path('customers/edit/<int:pk>/', views.CustomerUpdateView.as_view(), name='customer_edit'),
-    path('customers/delete/<int:pk>/', views.CustomerDeleteView.as_view(), name='customer_delete'),
-    # Including app's URLConf for any additional routing within 'myapp'
-    path('myapp/', include('myapp.urls')),
+    path('customers/', views.CustomerList.as_view(), name='customer_list'),
+    path('customers/add/', views.CustomerAdd.as_view(), name='customer_add'),
+    path('customers/edit/<int:pk>/', views.CustomerUpdate.as_view(), name='customer_edit'),
+    path('customers/delete/<int:pk>/', views.CustomerDelete.as_view(), name='customer_delete'),
+
 ]
--- GroupProject/skiwebsite/skiwebsite/urls.py
+++ GroupProject/skiwebsite/skiwebsite/urls.py
@@ -14,23 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
-from . import views
-
-app_name = 'skistore'  # This is for namespacing your app's URLs
+from django.urls import path, include
 
 urlpatterns = [
-    # Employee URLs
-    path('employees/', views.EmployeeListView.as_view(), name='employee_list'),
-    path('employees/add/', views.EmployeeAddView.as_view(), name='employee_add'),
-    path('employees/edit/<int:pk>/', views.EmployeeUpdateView.as_view(), name='employee_edit'),
-    path('employees/delete/<int:pk>/', views.EmployeeDeleteView.as_view(), name='employee_delete'),
-
-    # Customer URLs
-    path('customers/', views.CustomerListView.as_view(), name='customer_list'),
-    path('customers/add/', views.CustomerAddView.as_view(), name='customer_add'),
-    path('customers/edit/<int:pk>/', views.CustomerUpdateView.as_view(), name='customer_edit'),
-    path('customers/delete/<int:pk>/', views.CustomerDeleteView.as_view(), name='customer_delete'),
+    path('mainwebsite/', include("mainwebsite.urls")),
+    
 ]
-
 
